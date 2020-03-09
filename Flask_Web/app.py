6ba6268a1c37b3ef3bae9438beb7a92aae859c6e
adityatari <adityatari1@gmail.com>
2020-03-09 19:22:38 +0530

    table

--- Flask_Web/app.py
+++ Flask_Web/app.py
@@ -1,10 +1,14 @@
+#open anaconda base environment and app.py in it
 import os
-from flask import Flask, request, render_template, url_for, redirect
+from flask import Flask, flash, request, render_template, url_for, redirect
+#from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
+from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
-
+#app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test.db'
+#rom flask_sqlalchemy import SQLAlchemydb = SQLAlchemy(app)
 
  
 
@@ -12,18 +16,27 @@ app = Flask(__name__)
 def fileFrontPage():
     return render_template('index.html')
 
+
+
 @app.route("/handleUpload", methods=['POST'])
 def handleFileUpload():
     if 'photo' in request.files:
         photo = request.files['photo']
-        if photo.filename != '':            
-            photo.save(os.path.join('C:/Users/Public/Pictures', photo.filename))
+        if photo.filename != '': 
+            path = os.path.join('/home/aditya/Documents/Uploads/', '1.csv')
+            photo.save(path)
     return redirect(url_for('fileFrontPage'))
+
+@app.route("/suggest")
+def suggest():
+    return render_template('display.html')
 	
 @app.route("/display")
 def Display():
-
- return render_template("display.html")
+    df = pd.read_csv('/home/aditya/Documents/Uploads/1.csv', sep=',')
+    sf = df.head(5)
+    i = list(df.columns.values)
+    return render_template('display.html', tables=[sf.to_html()], index=i)
 
 @app.route('/history')
 def history():
